975cce07eec915715ff6a61eb8a6306b0fef2391
Nathan <expitau@gmail.com>
2024-07-13 17:44:33 +0000

    Preserve permissions home

--- lib/homesetup.sh
+++ lib/homesetup.sh
@@ -4,7 +4,7 @@
 if [ ! -d "/var/home" ] || [ -z "$(ls -A /var/home)" ]; then
   echo "/var/home doesn't exist or is empty. Copying /usr/homesetup to /var/home..."
   mkdir -p /var/home
-  cp -r /usr/homesetup/* /var/home
+  cp -rp /usr/homesetup/* /var/home
   echo "Copy complete."
 else
   echo "/var/home exists and is not empty. No files were copied."
--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 cd ~/System && \
-git pull && podman build --file ~/System/Containerfile --cap-add=SYS_ADMIN --security-opt label=disable --tag archlinux/latest && \
+git pull && podman build --file ~/System/Containerfile --no-hosts --no-hostname --cap-add=SYS_ADMIN --security-opt label=disable --tag archlinux/latest && \
 mkdir /var/rootfs && \
 podman export $(podman create archlinux/latest bash) > /var/rootfs.tar && tar -xf /var/rootfs.tar -C /var/rootfs && \
 ostree commit --branch=archlinux/latest --tree=dir=/var/rootfs && \
