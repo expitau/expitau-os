2254ebc5d7282f1daefc57a8c04073867a472fa7
Nathan <expitau@gmail.com>
2025-02-05 19:09:50 +0000

    Change mkinitcpio and fix baobab typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,7 @@
 set -e
 
 pacstrap -cKNP /mnt base base-devel linux linux-firmware grub efibootmgr nano git networkmanager
-cp /scripts/initramfs_overlay_hook /mnt/etc/initcpio/hooks/overlay_root
-cp /scripts/initramfs_overlay_install /mnt/etc/initcpio/install/overlay_root
-sed -i 's/^\(HOOKS=.*fsck\))/\1 overlay_root)/' /mnt/etc/mkinitcpio.conf
+cp /scripts/linux.preset /mnt/etc/mkinitcpio.d/linux.preset
 cp /scripts/profile.sh /mnt
 
 mkdir -p /mnt/var/cache/pacman
--- scripts/profile.sh
+++ scripts/profile.sh
@@ -15,7 +15,7 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 echo "nathan:test" | chpasswd
 pacman -S --noconfirm \
-    baobap gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-connections gnome-console gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-text-editor gnome-user-docs gnome-user-share gnome-weather gvfs gvfs-google loupe nautilus snapshot sushi xdg-desktop-portal-gnome totem \
+    baobab gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-connections gnome-console gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-text-editor gnome-user-docs gnome-user-share gnome-weather gvfs gvfs-google loupe nautilus snapshot sushi xdg-desktop-portal-gnome totem \
     code discord
 systemctl enable gdm
 
