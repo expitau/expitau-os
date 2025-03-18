1d8c39af29396a31bad6748bc8ff7dd135cf79c1
Nathan <expitau@gmail.com>
2025-03-17 21:00:10 -0400

    Switch to su nathan

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -34,11 +34,11 @@ pacman -S --noconfirm \
 
 # Install AUR packages
 mkdir -p /tmp/aur
-chown nobody:nobody /tmp/aur
+chown $SYSTEM_USER:$SYSTEM_USER /tmp/aur
 chmod 644 /etc/pacman.conf
 for pkg in paru-bin visual-studio-code-bin; do
-    sudo -u nobody -n -- git clone https://aur.archlinux.org/$pkg.git /tmp/aur/$pkg --depth 1
-    sudo -u nobody -n -- makepkg -D /tmp/aur/$pkg
+    su - $SYSTEM_USER -c "git clone https://aur.archlinux.org/$pkg.git /tmp/aur/$pkg --depth 1"
+    su - $SYSTEM_USER -c "makepkg -D /tmp/aur/$pkg"
 done
 
 pacman -U $(find . -type f -name "*.pkg.tar.zst" ! -name "*debug*" -print) --noconfirm
