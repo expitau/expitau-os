db6e6f876abf4de5042ee59e3e333b0fa3a59483
Nathan <expitau@gmail.com>
2025-03-23 13:08:01 -0400

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,41 @@
-FROM archlinux:base-devel
+ARG SYSTEM_USER=user
+ARG SYSTEM_PW=JHkkajlUJFI3V3dTdVYxbEsxTWdhWlFlVjBsbzAkTkMyaTNjd2ovZnVvZE84UXN4NlptblFWaWhFeE1sa0xjV0dWcmw3UGRyNgo=
+
+FROM archlinux:base-devel as iso
+
+# Copy host's pacman cache if not initialized
+RUN --mount=type=bind,source=./cache,target=/tmp/cache ls /tmp/cache
+# RUN --mount=type=bind,source=./cache,target=/tmp/cache \
+#     --mount=type=cache,target=/var/cache/pacman/pkg \
+#     [ "\$(ls -A /var/cache/pacman/pkg)" ] \
+#         && echo "Cache already exists: $(du -sh /var/cache/pacman/pkg | awk '{print $1}')" \
+#         || (echo "Cache is empty. Populating from host's ./cache." && cp -r /tmp/cache/* /var/cache/pacman/pkg || true)
 
 RUN pacman -Syu --noconfirm
-RUN pacman -S reflector --noconfirm
-RUN reflector --latest 5 --country CA --country US --sort rate --save /etc/pacman.d/mirrorlist
-RUN pacman -S arch-install-scripts squashfs-tools reflector rust git --noconfirm
+
+RUN pacman -S arch-install-scripts rust git --noconfirm
 
 RUN echo "root:0:1000" >> /etc/subuid && echo "root:0:1000" >> /etc/subgid
 
-RUN git clone https://github.com/expitau/expitau-os /src
+COPY system /src/system
 RUN cd /src/system && cargo build --release
 
 COPY scripts /scripts
 
-CMD bash scripts/build.sh
+RUN --mount=type=cache,target=/var/cache/pacman/pkg SYSTEM_USER=${SYSTEM_USER} SYSTEM_PW=${SYSTEM_PW} bash scripts/build.sh
+
+# FROM scratch as chroot
+
+# COPY --from=iso /mnt /
+
+# RUN --mount=type=cache,target=/var/cache/pacman/pkg SYSTEM_USER=${SYSTEM_USER} SYSTEM_PW=${SYSTEM_PW} bash /setup.sh
+
+# RUN rm /setup.sh
+
+# FROM archlinux:base-devel as image
+
+# COPY --from=chroot / /mnt
+
+# RUN pacman -S squashfs-tools --noconfirm
+
+# RUN mksquashfs /mnt /arch.sqfs
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,87 +1,84 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Set USER variable if not set, this comes from build script. Default password is not a secret
-SYSTEM_USER=${SYSTEM_USER:-user}
-SYSTEM_PW=${SYSTEM_PW:-JHkkajlUJFI3V3dTdVYxbEsxTWdhWlFlVjBsbzAkTkMyaTNjd2ovZnVvZE84UXN4NlptblFWaWhFeE1sa0xjV0dWcmw3UGRyNgo=}
+echo $SYSTEM_USER
+# # Install base system and packages required for setup (remaining packages will be installed as part of setup)
+# pacstrap -cKP /mnt base base-devel linux linux-firmware linux-headers sof-firmware git networkmanager wget
 
-# Install base system and packages required for setup (remaining packages will be installed as part of setup)
-pacstrap -cKNP /mnt base base-devel linux linux-firmware linux-headers sof-firmware git networkmanager wget
+# # Configure mkinitcpio to generate UKI images
+# cp /scripts/config/linux.preset /mnt/etc/mkinitcpio.d/linux.preset
+# cp /scripts/config/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
 
-# Configure mkinitcpio to generate UKI images
-cp /scripts/config/linux.preset /mnt/etc/mkinitcpio.d/linux.preset
-cp /scripts/config/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
+# # Copy example boot entry
+# cp /scripts/config/boot-entry.conf /mnt/etc/boot-entry.conf
 
-# Copy example boot entry
-cp /scripts/config/boot-entry.conf /mnt/etc/boot-entry.conf
+# # Copy fstab
+# cp /scripts/config/fstab /mnt/etc/fstab
 
-# Copy fstab
-cp /scripts/config/fstab /mnt/etc/fstab
+# # Copy nftables
+# cp /scripts/config/nftables.conf /mnt/etc/nftables.conf
 
-# Copy nftables
-cp /scripts/config/nftables.conf /mnt/etc/nftables.conf
+# # Copy tmpfile
+# sed "s/\$USER/$SYSTEM_USER/g" /scripts/config/data-tmpfile.conf > /mnt/etc/tmpfiles.d/00-data.conf
 
-# Copy tmpfile
-sed "s/\$USER/$SYSTEM_USER/g" /scripts/config/data-tmpfile.conf > /mnt/etc/tmpfiles.d/00-data.conf
+# # Copy desktop wallpaper
+# mkdir -p /mnt/usr/share/backgrounds/gnome
+# cp /scripts/config/wallpaper.png /mnt/usr/share/backgrounds/gnome/wallpaper.png
 
-# Copy desktop wallpaper
-mkdir -p /mnt/usr/share/backgrounds/gnome
-cp /scripts/config/wallpaper.png /mnt/usr/share/backgrounds/gnome/wallpaper.png
+# # Set user profile picture
+# mkdir -p /mnt/var/lib/AccountsService/{icons,users}
+# cp /scripts/config/icon.png /mnt/var/lib/AccountsService/icons/$SYSTEM_USER.png
+# cat <<EOF > /mnt/var/lib/AccountsService/users/$SYSTEM_USER
+# [User]
+# Session=
+# Icon=/var/lib/AccountsService/icons/$SYSTEM_USER
+# SystemAccount=false
+# EOF
 
-# Set user profile picture
-mkdir -p /mnt/var/lib/AccountsService/{icons,users}
-cp /scripts/config/icon.png /mnt/var/lib/AccountsService/icons/$SYSTEM_USER.png
-cat <<EOF > /mnt/var/lib/AccountsService/users/$SYSTEM_USER
-[User]
-Session=
-Icon=/var/lib/AccountsService/icons/$SYSTEM_USER
-SystemAccount=false
-EOF
+# # Copy shell config
+# cp /scripts/config/trueline.sh /mnt/etc/profile.d/trueline.sh
 
-# Copy shell config
-cp /scripts/config/trueline.sh /mnt/etc/profile.d/trueline.sh
+# # Copy dconf settings
+# mkdir -p /mnt/etc/dconf/db/local.d
+# mv /scripts/config/dconf.conf /mnt/etc/dconf/db/local.d/00-profile
 
-# Copy dconf settings
-mkdir -p /mnt/etc/dconf/db/local.d
-mv /scripts/config/dconf.conf /mnt/etc/dconf/db/local.d/00-profile
+# # Copy system source code
+# cp -r /src /mnt/usr/src/system
+# cp /src/system/target/release/system /mnt/usr/local/sbin/system
 
-# Copy system source code
-cp -r /src /mnt/usr/src/system
-cp /src/system/target/release/system /mnt/usr/local/sbin/system
+# cat <<EOF > /mnt/usr/src/system/.env
+# SYSTEM_USER=$SYSTEM_USER
 
-cat <<EOF > /mnt/usr/src/system/.env
-SYSTEM_USER=$SYSTEM_USER
+# # Encode: mkpasswd "password" | base64
+# # Decode: echo "ENCODED" | base64 -d
+# SYSTEM_PW=$SYSTEM_PW
+# EOF
 
-# Encode: mkpasswd "password" | base64
-# Decode: echo "ENCODED" | base64 -d
-SYSTEM_PW=$SYSTEM_PW
-EOF
+# # Copy pacman cache
+# # The package cache will be baked in to the image, so it will be available already after installation
+# # The cache will then be copied back to the host, so it can be reused for the next build
+# # mkdir -p /mnt/var/cache/pacman/pkg
+# # cp -R /var/cache/pacman/pkg/* /mnt/var/cache/pacman/pkg
 
-# Copy pacman cache
-# The package cache will be baked in to the image, so it will be available already after installation
-# The cache will then be copied back to the host, so it can be reused for the next build
-mkdir -p /mnt/var/cache/pacman/pkg
-cp -R /var/cache/pacman/pkg/* /mnt/var/cache/pacman/pkg
+# # # Setup mounts (because we aren't using arch-chroot)
+# # mount -t proc /proc /mnt/proc
+# # mount -t sysfs /sys /mnt/sys
+# # mount --rbind /dev /mnt/dev
+# cp /scripts/setup.sh /mnt/setup.sh
+# # cp /etc/resolv.conf /mnt/etc/resolv.conf
 
-# Setup mounts (because we aren't using arch-chroot)
-mount -t proc /proc /mnt/proc
-mount -t sysfs /sys /mnt/sys
-mount --rbind /dev /mnt/dev
-cp /scripts/setup.sh /mnt/setup.sh
-cp /etc/resolv.conf /mnt/etc/resolv.conf
+# # # Run setup script in chroot
+# # # These variables are passed in podman run with --env SYSTEM_USER=username --env SYSTEM_PW=password
+# # chroot /mnt env SYSTEM_USER=$SYSTEM_USER SYSTEM_PW=$SYSTEM_PW /bin/bash /setup.sh
 
-# Run setup script in chroot
-# These variables are passed in podman run with --env SYSTEM_USER=username --env SYSTEM_PW=password
-chroot /mnt env SYSTEM_USER=$SYSTEM_USER SYSTEM_PW=$SYSTEM_PW /bin/bash /setup.sh
+# # # Cleanup
+# # cp -R /mnt/var/cache/pacman/pkg/* /var/cache/pacman/pkg
+# # umount -l /mnt/proc
+# # umount -l /mnt/sys
+# # umount -l /mnt/dev
+# # rm /mnt/setup.sh
 
-# Cleanup
-cp -R /mnt/var/cache/pacman/pkg/* /var/cache/pacman/pkg
-umount -l /mnt/proc
-umount -l /mnt/sys
-umount -l /mnt/dev
-rm /mnt/setup.sh
+# mkdir -p /mnt/efi
 
-mkdir -p /mnt/efi
-
-# Create image
-mksquashfs /mnt /arch.sqfs
+# # # Create image
+# # mksquashfs /mnt /arch.sqfs
