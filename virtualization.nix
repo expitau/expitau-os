e8d62bef7840c157e6db26460313e7c0eba01782
Nathan <expitau@gmail.com>
2025-09-28 09:50:29 -0400

    Switch to generation

--- virtualization.nix
+++ virtualization.nix
@@ -20,4 +20,8 @@
   ];
 
   networking.firewall.trustedInterfaces = [ "virbr0" ];
+
+  home-manager.users.nathan.home.file = {
+    "Data/libvirt/edk-2-i386-vars.fd".source = "${pkgs.qemu}/share/qemu/edk2-i386-vars.fd";
+  }
 }
