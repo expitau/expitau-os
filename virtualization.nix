2516ed20f8d7e5d605814f5d7f1733159efd9974
Nathan <expitau@gmail.com>
2025-09-27 21:08:18 -0400

    Switch to generation

--- virtualization.nix
+++ virtualization.nix
@@ -18,4 +18,20 @@
   environment.systemPackages = with pkgs; [
     virt-viewer
   ];
+
+  networking.libvirt = {
+    networks."default" = {
+      # NAT mode (like virbr0 normally does)
+      forward.mode = "nat";
+      bridge = "virbr0";
+      addresses = [ {
+        address = "192.168.122.1";
+        prefixLength = 24;
+      } ];
+      dhcp.range = {
+        start = "192.168.122.2";
+        end = "192.168.122.254";
+      };
+    };
+  };
 }
